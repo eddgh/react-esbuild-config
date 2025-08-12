f9141a89adb8fad51afb075984335f89ece42473
Edmundo Jose Batista <eddmundosjb@gmail.com>
2025-08-12 15:07:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Meu projeto com esbuild :)</h1>
+      <h2>Testando reload</h2>
+      <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, nostrum est quaerat quasi magnam cum nulla doloribus officia sit. Facere eaque nulla corporis reprehenderit doloribus qui saepe, blanditiis quibusdam incidunt!</div>
+    </div>
+  );
+};
+
+export default App;
